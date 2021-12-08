09b028c8f8a748f96ea6c7a1a6ff6ebbeb719f9f
Francisco Andre <55844517+Isco170@users.noreply.github.com>
2021-12-08 23:10:49 +0200

    Admin app done

--- admin/src/controller/product.controller.js
+++ admin/src/controller/product.controller.js
@@ -1,9 +1,42 @@
-const product = require('../model/product.model');
+const prodModel = require('../model/product.model');
 
 async function getProducts(request, response){
-    const products = await product.findAll();
+    const products = await prodModel.findAll();
     return response.json(products)
 }
+
+async function newProduct(request, response){
+    const product = await prodModel.create(request.body);
+    return response.send(product)
+}
+
+async function getOne(request, response){
+    const product = await prodModel.findOne({ where:{ id: request.params.id}});
+    return response.send(product);
+}
+
+async function updateProd(request, response){
+    const prod = await prodModel.update(request.body, {where:{id: request.body.id}});
+    return response.send(prod)
+}
+
+async function deleteProd(request, response){
+    const prod = await prodModel.destroy({where:{id : request.params.id}});
+    return response.send(true);
+}
+
+async function likeProd(request, response){
+    const prod = await prodModel.findOne({where:{id : request.params.id}});
+
+    await prodModel.update({likes: prod.likes +1}, {where:{id: prod.id}});
+    return response.send(prod);
+}
+
  module.exports = {
-     getProducts
+     getProducts,
+     newProduct,
+     getOne,
+     updateProd,
+     deleteProd,
+     likeProd
  }
--- admin/src/route/product.routes.js
+++ admin/src/route/product.routes.js
@@ -3,5 +3,10 @@ let route = express.Router();
 const productController = require('../controller/product.controller');
 
 route.get('/', productController.getProducts);
+route.get('/:id', productController.getOne);
+route.post('/', productController.newProduct);
+route.put('/', productController.updateProd);
+route.delete('/:id', productController.deleteProd);
+route.post('/:id/like', productController.likeProd);
 
 module.exports = route;
