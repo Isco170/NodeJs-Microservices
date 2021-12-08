c48725f76ea9a84cc527a9f8d79e76d4d739aa70
Francisco Andre <55844517+Isco170@users.noreply.github.com>
2021-12-08 21:27:34 +0200

    Product entity

--- admin/src/app.ts
+++ admin/src/app.ts
@@ -1,10 +1,23 @@
-import express from 'express';
-import cors from 'cors';
+import * as  express from 'express';
+import { Request, Response} from 'express';
+import * as cors from 'cors';
+import {createConnection} from 'typeorm';
+import { Product } from './entity/product';
 
-const app = express();
+createConnection().then( db => {
+    const productRepository = db.getRepository(Product)
+    const app = express();
 
-app.use(cors());
+    app.use(cors());
+    
+    app.use(express.json());
 
-app.use(express.json());
+    app.get('/api/products', async (request: Request, response: Response) => {
+        const products = await productRepository.find();
 
-app.listen(8000)
+        return response.json(products);
+    })
+
+    console.log("Running")
+    app.listen(8000)
+})
